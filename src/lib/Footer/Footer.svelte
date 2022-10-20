602e037dd39241123c55d2cebd80000525037282
Kawaljeet2001 <batrakawaljeetsingh@gmail.com>
2022-10-20 22:08:42 +0530

    feat: created Footer component

---
+++ src/lib/Footer/Footer.svelte
@@ -0,0 +1,6 @@
+<footer class="flex flex-col items-center justify-center w-full bg-green1 py-8">
+	<p class="text-white text-sm font-light">Copyright © 2019-2022. All rights reserved</p>
+	<p class="mt-1 text-white text-sm font-light">
+		All brands, logos are trademarks or registered trademarks of their respective owners.
+	</p>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
 	import '../app.css';
+	import Footer from '../lib/Footer/Footer.svelte';
 	import Navbar from '../lib/Navbar/Navbar.svelte';
 </script>
 
 <Navbar />
 <slot />
+<Footer/>
