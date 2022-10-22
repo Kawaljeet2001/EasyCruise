7cc31de9fefc3dc29719e0d7c65677f14773d490
Kawaljeet2001 <batrakawaljeetsingh@gmail.com>
2022-10-22 21:44:40 +0530

    feat: created flight itineray page

---
+++ src/lib/Flights/FlightJourney.svelte
@@ -0,0 +1,17 @@
+<div class="bg-white grid grid-cols-12 rounded-lg w-full p-4 min-h-12v mb-2">
+	<div class="flex flex-col justify-center col-span-2">
+		<h3>GO FIRST</h3>
+		<p>G8-399</p>
+	</div>
+	<h3 class="flex items-center col-span-2">22:35</h3>
+	<p class="flex items-center col-span-2">1h 40min</p>
+	<h3 class="flex items-center col-span-2">00:15</h3>
+	<h3 class="flex items-center col-span-2">Rs. 6265</h3>
+	<div class="flex items-center col-span-2">
+		<button
+			class="w-full rounded-md text-sm h-8 bg-yellow1 text-white font-medium flex items-center justify-center p-2"
+			>Book</button
+		>
+	</div>
+	<div class="mt-4 col-span-12 border border-gray-200 rounded-sm h-15v p-2">grg</div>
+</div>
---
+++ src/routes/flights/itinerary/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import ReviewItinerary from '../../../lib/Flights/ReviewItinerary.svelte';
+</script>
+
+<div class="h-screen w-full flex justify-center">
+	<div class="w-8/12 py-12">
+		<ReviewItinerary />
+	</div>
+</div>
