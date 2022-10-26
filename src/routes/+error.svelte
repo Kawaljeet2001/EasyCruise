1de493dbaecce55a1e5d1995c8dd94ea41ba0c1a
Kawaljeet2001 <batrakawaljeetsingh@gmail.com>
2022-10-26 20:12:09 +0530

    feat: created the 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,28 @@
+<script>
+</script>
+
+<div class="h-screen absolute z-50 top-0 left-0 w-full bg-white grid grid-cols-12">
+	<div class="col-span-6 w-full" />
+	<div class="h-full w-full col-span-6 flex flex-col justify-center">
+		<p class="textlarge font-extrabold text-gray-800">404</p>
+		<p class="textlarge2 -mt-12 font-extrabold text-gray-800">Page not found!</p>
+		<p class="text-gray-500 font-medium text-3xl w-8/12 mt-6">
+			We're sorry, the page you requested could not be found. Please go back to the homepage!
+		</p>
+		<a
+			href="/"
+            rel="noreferrer"
+			class="bg-orange1 hover:bg-orange-700 px-6 py-4 text-xl rounded-md text-white font-bold flex items-center justify-center w-4/12 mt-8"
+			>Go Home</a
+		>
+	</div>
+</div>
+
+<style>
+	.textlarge {
+		font-size: 130px;
+	}
+	.textlarge2 {
+		font-size: 80px;
+	}
+</style>
